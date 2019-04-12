69126b4939117e45353db9f3591f743fb2e3a472
Jorge <jorge.nakassima@ibm.com>
2019-04-12 15:06:19 -0300

    create a select model and add demo file

--- demo.js
+++ demo.js
@@ -1,16 +1,9 @@
-# data-inputjs
-simple text input per terminal
+const input = require("./index")
 
-    npm i data-inputjs
-
-
-**Select exemple**
-```js
 const colorSelector = async () => {
     console.log("select a color")
     const select = await input.select(["red", 'blue', 'orange', 'pink'])
     console.log(`your color is ${select}`)
-
 
     // select a color
 
@@ -22,19 +15,14 @@ const colorSelector = async () => {
     // your color is blue
 
 }
-```
-**Input exemple**
-```js
+
+
 const addName = async () => {
     const name = await input.text("add your name: ")
     console.log(name)
 
-    // add your name: jorge
-    // jorge
+// add your name: jorge
+// jorge
 
 }
-```
-
-
-
 
